b0c051bf81675bd930f699f634aae91ff070c625
junayednoman <junayednoman05@gmail.com>
2025-08-27 18:40:17 +0600

    fix: new cors origin added

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import routeNotFound from "./app/middlewares/routeNotFound";
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://147.182.247.218:3003", "https://huemanexpressions.com/"],
+  origin: ["http://localhost:3000", "http://147.182.247.218:3003", "https://huemanexpressions.com", "https://www.huemanexpressions.com"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   credentials: true,
