8bff2348b05259323f8a41bbe3c7ad90a3e79400
junayednoman <junayednoman05@gmail.com>
2026-08-22 13:23:16 +0600

    fix: dashboard domain added to cors

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(
       "https://portiapro.huemanexpressions.com",
       "http://10.10.10.78:3000",
       "http://10.10.28.34:3000",
-      "http://167.71.243.194:3005"
+      "http://dashboard.huemanexpressions.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
