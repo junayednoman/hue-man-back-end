7b9e65204f69890299ef7c165680578bd558b736
junayednoman <junayednoman05@gmail.com>
2026-08-22 10:11:05 +0600

    fix: cors origin added, category update logic changed

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -18,12 +18,18 @@ const createCategory = async (payload: TCategory) => {
 
 const createManyCategories = async (payload: TCategory[]) => {
   const cateNames = payload.map((cat) => cat.name);
-  const categories = await CategoryModel.find({ name: { $in: cateNames } })
+  const categories = await CategoryModel.find({ name: { $in: cateNames } });
   if (categories && categories.length > 0) {
     const existingCategoryNames = categories.map((cat) => cat.name);
-    const newCategories = payload.filter((cat) => !existingCategoryNames.includes(cat.name));
+    const newCategories = payload.filter(
+      (cat) => !existingCategoryNames.includes(cat.name),
+    );
     const result = await CategoryModel.insertMany(newCategories);
-    const existingMessage = categories.length == 1 ? `A category already exists with the name: ${categories[0].name}` : "Following categories already exist: " + existingCategoryNames.join(", ")
+    const existingMessage =
+      categories.length == 1
+        ? `A category already exists with the name: ${categories[0].name}`
+        : "Following categories already exist: " +
+          existingCategoryNames.join(", ");
     return { data: result, message: existingMessage };
   }
 
@@ -32,13 +38,11 @@ const createManyCategories = async (payload: TCategory[]) => {
 };
 
 const getAllCategories = async (query: Record<string, any>) => {
-  const searchableFields = [
-    "name",
-  ];
-  query.limit = 100000
+  const searchableFields = ["name"];
+  query.limit = 100000;
   const categoryQuery = new QueryBuilder(
     CategoryModel.find({ parent: null }),
-    query
+    query,
   )
     .search(searchableFields)
     .filter()
@@ -63,7 +67,7 @@ const updateCategory = async (
     name?: string;
     image?: string;
     index?: number;
-  }
+  },
 ) => {
   const category = await CategoryModel.findOne({ _id, is_deleted: false });
   if (!category) {
@@ -86,18 +90,12 @@ const updateCategory = async (
 const deleteCategory = async (_id: string) => {
   const category = await CategoryModel.findById(_id);
   if (!category) throw new AppError(404, "Category not found");
-  if (category.is_deleted) throw new AppError(400, "Category already deleted");
 
-  const result = await CategoryModel.findOneAndUpdate(
-    { _id },
-    { is_deleted: true },
-    { new: true }
-  );
+  const result = await CategoryModel.findOneAndDelete({ _id });
 
-  // if (result) {
-  //   const objectKey = category.image.split(".com/")[1];
-  //   deleteImageFromS3(objectKey);
-  // }
+  if (result) {
+    await deleteFile(category.image);
+  }
 
   return result;
 };
@@ -108,6 +106,6 @@ const categoryServices = {
   getSingleCategory,
   updateCategory,
   deleteCategory,
-  createManyCategories
+  createManyCategories,
 };
 export default categoryServices;
